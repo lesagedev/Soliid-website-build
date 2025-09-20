2a4a7cf95576819673608c1a9d298fcbc9d54230
Marketer <ldogmo@buildermats.com>
2025-09-20 16:31:13 +0100

    Feat : Add SEO logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 export const metadata: Metadata = {
-  title: "Soliid - Préfabrication de Parpaings, Pavés et Bordures au Cameroun",
+  title: "Soliid - Préfabrication de Parpaings, Pavés et Bordures",
   description:
     "Soliid, votre partenaire de confiance pour la préfabrication de parpaings, pavés et bordures au Cameroun. Construire facile avec nos solutions de qualité à Douala et dans tout le Cameroun.",
   keywords:
@@ -17,23 +17,61 @@ export const metadata: Metadata = {
   publisher: "Soliid",
   robots: "index, follow",
   openGraph: {
-    title: "Soliid - Préfabrication de Parpaings et Pavés au Cameroun",
+    title: "Soliid - Préfabrication de Parpaings, Pavés et Bordures",
     description:
       "Votre partenaire de confiance pour la préfabrication de parpaings et pavés au Cameroun. Construire facile avec nos solutions de qualité.",
-    url: "https://soliid.cm",
+    url: "https://www.soliid-cm.com",
     siteName: "Soliid",
     locale: "fr_FR",
     type: "website",
+    images: [
+      {
+        url: "/logo/logo-seo.png",
+        width: 1024,
+        height: 1024,
+        alt: "Logo Soliid avec M. SOLIID"
+      },
+      {
+        url: "/logo/logo-soliid.png",
+        width: 782,
+        height: 281,
+        alt: "Logo Soliid"
+      },
+      {
+        url: "/home/parpaing-soliid.png",
+        width: 960,
+        height: 1280,
+        alt: "Palettes contenant des Parpaings de la marque Soliid"
+      },
+      {
+        url: "/home/20190912_155144.jpg",
+        width: 3096,
+        height: 4128,
+        alt: "Usine de préfabrication de parpaings et pavés Soliid"
+      },
+      {
+        url: "/home/mascot-soliid.png",
+        width: 1024,
+        height: 1536,
+        alt: "Mascotte Soliid tenant un parpaing"
+      },
+      {
+        url: "/home/mascot-soliid-paves.png",
+        width: 2812,
+        height: 4046,
+        alt: "Mascotte Soliid tenant un pavé"
+      }
+    ]
   },
   twitter: {
     card: "summary_large_image",
     title: "Soliid - Préfabrication de Parpaings et Pavés au Cameroun",
     description: "Votre partenaire de confiance pour la préfabrication de parpaings et pavés au Cameroun.",
+    images: ["/logo/logo-seo.png", '/home/parpaing-soliid.png', '/home/mascot-soliid.png', '/home/mascot-soliid-paves.png'],
   },
   verification: {
     google: "your-google-verification-code",
-  },
-  generator: 'v0.app'
+  }
 }
 
 export default function RootLayout({
